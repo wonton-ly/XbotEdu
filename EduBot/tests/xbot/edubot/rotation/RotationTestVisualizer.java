da8bd7ed1be2a5d79e4ef8ee73e5ccc8c8f421c5
WasabiFan <wasabifan@outlook.com>
2016-01-23 23:46:37 -0800

    Refactor visualization code and prepare for selectable tests

--- EduBot/tests/xbot/edubot/rotation/RotationTestVisualizer.java
+++ EduBot/tests/xbot/edubot/rotation/RotationTestVisualizer.java
@@ -22,6 +22,7 @@ import javax.swing.BoxLayout;
 
 public class RotationTestVisualizer {
     private JFrame frame;
+    private RotationVisualizationPanel vizPanel;
     BaseOrientationEngineTest currentTestEnvironment;
     RotationEnvironmentState envState = new RotationEnvironmentState();
 
@@ -57,59 +58,49 @@ public class RotationTestVisualizer {
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.getContentPane().setLayout(new BoxLayout(frame.getContentPane(), BoxLayout.Y_AXIS));
 
-        JPanel vizPanel = new JPanel() {
-            private int centerX;
-            private int centerY;
-            private XYPair center;
-            private double baseMagnitude;
-
-            @Override
-            protected void paintComponent(Graphics g) {
-                super.paintComponent(g);
-                Graphics2D graphics = (Graphics2D) g;
-
-                centerX = this.getWidth() / 2;
-                centerY = this.getHeight() / 2;
-                center = new XYPair(centerX, centerY);
-                baseMagnitude = Math.min(centerX, centerY) * 0.8;
-
-                graphics.setStroke(new BasicStroke(5));
-                graphics.setColor(Color.BLUE);
-                drawVector(graphics, XYPair.fromPolar(envState.targetOrientation, baseMagnitude));
-
-                graphics.setColor(Color.BLACK);
-                XYPair currentOrientationVector = XYPair.fromPolar(envState.currentOrientation, baseMagnitude);
-                drawVector(graphics, currentOrientationVector);
-
-                graphics.setColor(Color.CYAN);
-                drawVector(graphics, XYPair.fromPolar(envState.currentOrientation + 90,
-                        envState.currentRotationalPower * 0.5 * baseMagnitude), currentOrientationVector);
-                
-                graphics.setColor(Color.GRAY);
-                drawVector(graphics, XYPair.fromPolar(envState.currentOrientation + 90,
-                        envState.currentVelocity / 30 * baseMagnitude), currentOrientationVector.scale(0.5));
-            }
-
-            protected void drawVector(Graphics2D graphics, XYPair vector) {
-                drawVector(graphics, vector, XYPair.ZERO);
-            }
-
-            protected void drawVector(Graphics2D graphics, XYPair vector, XYPair originPosition) {
-                XYPair centeredOrigin = originPosition.clone().add(center);
-                XYPair positionedVector = vector.clone().add(centeredOrigin);
-                graphics.drawLine((int)centeredOrigin.x, (int)centeredOrigin.y, (int) positionedVector.x, (int) positionedVector.y);
-            }
-        };
+        vizPanel = new RotationVisualizationPanel();
+        vizPanel.updateState(envState);
+        
         frame.getContentPane().add(vizPanel);
+        
 
-        currentTestEnvironment = new GoToOrientationTest();
+        setRotationTest(OrientationTest.ROTATE_TO_ORIENTATION);
+    }
+    
+    private void setRotationTest(OrientationTest test) {
+        switch(test) {
+            case GO_LEFT_90_FROM_0:
+            case GO_LEFT_90_FROM_150:
+            case GO_LEFT_90_FROM_NEG_150:
+            case GO_LEFT_90_FROM_NEG_90:
+                currentTestEnvironment = new TurnLeft90DegreesCommandTest();
+                break;
+            case ROTATE_TO_ORIENTATION:
+                currentTestEnvironment = new GoToOrientationTest();
+                break;
+        }
+        
         currentTestEnvironment.setUp();
         
         currentTestEnvironment.setAsAsync((BaseOrientationEngineTest.RotationEnvironmentState envState) -> {
             this.envState = envState;
+            vizPanel.updateState(envState);
             vizPanel.repaint();
         });
+        
+        ((SelectableOrientationTest) currentTestEnvironment).invokeOrientationTest(test);
+        
+    }
 
-        ((GoToOrientationTest) currentTestEnvironment).testGoToOrientation();
+    public static enum OrientationTest {
+        GO_LEFT_90_FROM_0,
+        GO_LEFT_90_FROM_NEG_90,
+        GO_LEFT_90_FROM_NEG_150,
+        GO_LEFT_90_FROM_150,
+        ROTATE_TO_ORIENTATION
+    }
+    
+    public static interface SelectableOrientationTest {
+        void invokeOrientationTest(OrientationTest test);
     }
 }
---
+++ EduBot/tests/xbot/edubot/rotation/RotationVisualizationPanel.java
@@ -0,0 +1,61 @@
+package xbot.edubot.rotation;
+
+import java.awt.BasicStroke;
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+
+import javax.swing.JPanel;
+
+import xbot.common.math.XYPair;
+import xbot.edubot.rotation.BaseOrientationEngineTest.RotationEnvironmentState;
+
+public class RotationVisualizationPanel extends JPanel {
+    private int centerX;
+    private int centerY;
+    private XYPair center;
+    private double baseMagnitude;
+    
+    RotationEnvironmentState envState = new RotationEnvironmentState();
+    
+    public void updateState(RotationEnvironmentState envState) {
+        this.envState = envState;
+    }
+    
+    @Override
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        Graphics2D graphics = (Graphics2D) g;
+
+        centerX = this.getWidth() / 2;
+        centerY = this.getHeight() / 2;
+        center = new XYPair(centerX, centerY);
+        baseMagnitude = Math.min(centerX, centerY) * 0.8;
+
+        graphics.setStroke(new BasicStroke(5));
+        graphics.setColor(Color.BLUE);
+        drawVector(graphics, XYPair.fromPolar(envState.targetOrientation, baseMagnitude));
+
+        graphics.setColor(Color.BLACK);
+        XYPair currentOrientationVector = XYPair.fromPolar(envState.currentOrientation, baseMagnitude);
+        drawVector(graphics, currentOrientationVector);
+
+        graphics.setColor(Color.CYAN);
+        drawVector(graphics, XYPair.fromPolar(envState.currentOrientation + 90,
+                envState.currentRotationalPower * 0.5 * baseMagnitude), currentOrientationVector);
+        
+        graphics.setColor(Color.GRAY);
+        drawVector(graphics, XYPair.fromPolar(envState.currentOrientation + 90,
+                envState.currentVelocity / 30 * baseMagnitude), currentOrientationVector.scale(0.5));
+    }
+
+    protected void drawVector(Graphics2D graphics, XYPair vector) {
+        drawVector(graphics, vector, XYPair.ZERO);
+    }
+
+    protected void drawVector(Graphics2D graphics, XYPair vector, XYPair originPosition) {
+        XYPair centeredOrigin = originPosition.clone().add(center);
+        XYPair positionedVector = vector.clone().add(centeredOrigin);
+        graphics.drawLine((int)centeredOrigin.x, (int)centeredOrigin.y, (int) positionedVector.x, (int) positionedVector.y);
+    }
+}
