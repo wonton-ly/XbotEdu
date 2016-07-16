0517dae81fbacd35bb0b1448bab6a608b40fc744
John Gilbert <johngilb@gmail.com>
2016-07-16 14:50:16 -0700

    Add the class level variables for TankDriveWithJoysticksCommand.java along with their initializers in constructor. We'll give people the opportunity to do this themselves later, but at first we should do a little more hand-holding.

--- EduBot/src/xbot/edubot/subsystems/drive/commands/TankDriveWithJoysticksCommand.java
+++ EduBot/src/xbot/edubot/subsystems/drive/commands/TankDriveWithJoysticksCommand.java
@@ -8,9 +8,13 @@ import xbot.edubot.subsystems.drive.DriveSubsystem;
 
 public class TankDriveWithJoysticksCommand extends BaseCommand {
 
+	DriveSubsystem drive;
+	OperatorInterface operate;
+	
 	@Inject
 	public TankDriveWithJoysticksCommand(DriveSubsystem driveSubsystem, OperatorInterface oi) {
-		// TODO Auto-generated constructor stub
+		drive = driveSubsystem;
+		operate = oi;
 	}
 	
 	@Override
