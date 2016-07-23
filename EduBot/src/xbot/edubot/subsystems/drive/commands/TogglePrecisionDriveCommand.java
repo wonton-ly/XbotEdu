77d8558ba78f43fb9441f2ce12ebb4b385af7c78
John Gilbert <johngilb@gmail.com>
2016-07-23 13:23:44 -0700

    fix typo

--- EduBot/src/xbot/edubot/subsystems/drive/commands/TogglePrecisionDriveCommand.java
+++ EduBot/src/xbot/edubot/subsystems/drive/commands/TogglePrecisionDriveCommand.java
@@ -21,7 +21,7 @@ public class TogglePrecisionDriveCommand extends BaseCommand {
 		// major ways to do this:
 		// 1) Create something like GetPrecisionMode() and SetPrecisionMode() on the DriveSubsystem. Your
 		//    command can read the current value, change it, and set it back into the subsystem.
-		// 2) Create a TogglePrecisoinMode() on the DriveSubsystem. The system will internally change
+		// 2) Create a TogglePrecisionMode() on the DriveSubsystem. The system will internally change
 		//    the current mode.
 		// In all of these cases you'll need to have the mode somehow affect the TankDrive method.
 	}
