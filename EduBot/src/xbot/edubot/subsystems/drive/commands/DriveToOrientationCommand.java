d403e8918f014c4d6d73a909d45d7ac1d6e54d3e
John Gilbert <johngilb@gmail.com>
2016-08-06 11:05:35 -0700

    Added more comments to DriveToOrientationCommand, giving some basic instructions

--- EduBot/src/xbot/edubot/subsystems/drive/commands/DriveToOrientationCommand.java
+++ EduBot/src/xbot/edubot/subsystems/drive/commands/DriveToOrientationCommand.java
@@ -15,20 +15,28 @@ public class DriveToOrientationCommand extends BaseCommand{
 	}
 	
 	public void setTargetHeading(double heading) {
-		
+		// This method will be called by the test, and will give you a goal heading.
+		// You'll need to remember this target position and use it in your calculations.
 	}
 	
 	@Override
 	public void initialize() {
-		// TODO Auto-generated method stub
-		
+		// If you have some one-time setup, do it here.		
 	}
 
 	@Override
 	public void execute() {
-		// TODO Auto-generated method stub
+		// Here you'll need to figure out a technique that:
+		// - Gets the robot to turn to the target orientation
+		// - Gets the robot stop (or at least be moving really really slowly) at the target position
+		
+		// How you do this is up to you. If you get stuck, ask a mentor or student for some hints!
 	}
 
-
-
+	@Override
+	public boolean isFinished() {
+		// Modify this to return true once you have met your goal, 
+		// and you're moving fairly slowly (ideally stopped)
+		return false;
+	}
 }
