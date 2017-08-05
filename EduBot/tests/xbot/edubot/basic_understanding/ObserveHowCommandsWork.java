99141dc4b05188b16007d4b660fbcf13cb13841f
John Gilbert <johngilb@gmail.com>
2017-08-05 13:23:49 -0700

    Add some newer commands that teach basic concepts

--- EduBot/tests/xbot/edubot/basic_understanding/ObserveHowCommandsWork.java
+++ EduBot/tests/xbot/edubot/basic_understanding/ObserveHowCommandsWork.java
@@ -9,13 +9,37 @@ public class ObserveHowCommandsWork extends BaseWPITest{
 
 	@Test
 	public void watch() {
+		// This is a simple command that just prints out what methods are called on it.
 		ExampleCommand cmd = injector.getInstance(ExampleCommand.class);
 		
+		// There's many ways to start commands. Typically, they are started by pressing a joystick
+		// button, but for testing we can just force it to start.
 		cmd.start();
 		
+		// The Scheduler is an entity that looks at all "Started commands" and runs them until:
+		// - the command says it is done
+		// - some other command starts that wants to use the same subsystem
 		XScheduler scheduler = injector.getInstance(XScheduler.class);
 		
+		// For a quick example, we'll just run the scheduler 10 times.
 		for (int i = 0; i < 10; i++) {
+			scheduler.run();
+		}
+	}
+	
+	@Test
+	public void watchChatter() {
+		ChatCommandThatEnds cmd = injector.getInstance(ChatCommandThatEnds.class);
+		cmd.start();
+		XScheduler scheduler = injector.getInstance(XScheduler.class);
+		
+		for (int i = 0; i < 100; i++) {
+			
+			if (i % 10 == 0) 
+			{
+				System.out.println("Scheduler on loop" + i);
+			}
+			
 			scheduler.run();
 		}
 	}
